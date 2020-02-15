bb8bec5856233b5827c87a03deedd1881929fe0d
francoisparizad <francoisparizad@localhost>
2020-02-15 11:20:48 +0100

    15/02/2020

---
+++ https:/github.com/666Ahriman666/Francois.git/cm2020/src/premier_cours/Bonjour.java
@@ -0,0 +1,11 @@
+package premier_cours;
+
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ https:/github.com/666Ahriman666/Francois.git/cm2020/src/premier_cours/Bonjourtoutlemonde.java
@@ -0,0 +1,11 @@
+package premier_cours;
+
+
+public class Bonjourtoutlemonde {
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour tout le monde");
+
+	}
+
+}
